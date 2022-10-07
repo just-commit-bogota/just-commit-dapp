60d2205ac467e143308f8a2f57967a486f51267b
Daniel Belfort <daniel@belfort.com>
2022-10-07 08:36:38 +0000

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,44 @@
+import '../styles/globals.css'
+import "@rainbow-me/rainbowkit/styles.css"
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+import PlausibleProvider from 'next-plausible'
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.goerli],
+  [alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+});
+
+// always false until I register .xyz domain
+const isProdEnv = process.env.NEXT_PUBLIC_VERCEL_ENV == 'production'
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <PlausibleProvider
+            domain={isProdEnv ? 'prod-xyz-here' : 'phone-awareness-dapp.danielbelfort.repl.co'}
+            trackLocalhost={!isProdEnv}
+            trackOutboundLinks
+        >
+          <Component {...pageProps} />
+        </PlausibleProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  )
+}
+
+export default App
---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import toast, { Toaster } from 'react-hot-toast'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width" />
+        <title>dApp</title>
+        <meta property="og:title" content="dApp" />
+        <meta name="description" content="dApp" />
+        <meta property="og:description" content="dApp" />
+      </Head>
+
+      <div className="bg-gradient-to-b from-emerald-100 to-white-500 min-h-screen">
+        <div className="home-page mx-auto py-3">
+          <div className="container mx-auto flex items-center justify-between absolute mt-6 ml-6">
+            <div className="text-4xl cursor-default text-black font-semibold ml-6">
+              dApp
+            </div>
+            <div>
+              <ConnectButton className="mr-2 md:inline-flex hover:shadow-lg flex " />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
