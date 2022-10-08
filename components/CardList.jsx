35d87148776bb2f2d3d18cdd52b102e62e7bfdc2
Monta Gao <mzgao@uwaterloo.ca>
2022-10-08 17:26:22 -0500

    CardList

--- components/CardList.jsx
+++ components/CardList.jsx
@@ -1,12 +1,24 @@
 import React  from 'react'
 import classNames from 'classnames'
-
-
+import CommitCard from "./CommitCard"
 
 // this component displays all the commitment cards in a sorted list
 // based on expiry time and urgency
-function CardList(props) {
+export default function CardList({ cardList }) {
+	if (!cardList) {
+		return null;
+	}
 
+	return (
+    <div>
+			{cardList.map((card) => (
+				<CommitCard 
+                    key={wave.timestamp} 
+                    address={wave.address}
+                    timestamp={wave.timestamp}
+                    message={wave.message}
+				/>
+			)).reverse()}
+		</div>
+	);
 }
-
-export default CardList;
