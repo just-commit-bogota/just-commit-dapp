8cf0c057bbbd78ceb9ae67e7cceec54a8f9fe1eb
Daniel Belfort <daniel@belfort.com>
2022-10-09 00:01:06 +0000

    clean before merge

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,36 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useState } from 'react'
+import abi from "../contracts/CommitManager.json";
+import { ethers } from 'ethers'
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
 import Button from '@mui/material/Button';
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import toast, { Toaster } from 'react-hot-toast'
+import { useAccount, useNetwork, useProvider } from 'wagmi'
 
 export default function Home() {
+
+  // state variables
+  const [dialogOpen, setDialogOpen] = useState(false)
+  const [commitDescription, setCommitDescription] = useState('');
+  const [commitTo, setCommitTo] = useState('');
+  const [commitAmount, setCommitAmount] = useState(0)
+  const [validThrough, setValidThrough] = useState(0)
+
+  // smart contract data
+  const provider = useProvider()
+  const { chain, chains } = useNetwork()
+  const { address: isConnected } = useAccount()
+  const contractAddress = "0x28D691d5eDFf71b72B8CA60EDcB164308945707F"
+  const commitManagerContract = new ethers.Contract(
+    contractAddress,
+    abi.abi,
+    provider
+  )
+
   return (
     <>
       <Head>
@@ -37,24 +60,28 @@ export default function Home() {
           className="form"
           onSubmit={async (e) => {
             e.preventDefault()
-          }
-        }
+            // do Toast checks 
+            setDialogOpen(true)
+          }}      
         >
           <div className="col">
             <TextField
               id="outlined-helperText"
               placeholder="focused building"
               helperText="Description"
+              onChange={(e) => setCommitDescription(e.target.value)}
             />
             <TextField
               id="outlined-helperText"
               placeholder="0xb44...aad"
               helperText="Commit To"
+              onChange={(e) => setCommitTo(e.target.value)}
             />
             <TextField
               id="outlined-helperText"
               placeholder="0.001"
-              helperText="Deposit Amount"
+              helperText="Commit Amount"
+              onChange={(e) => setCommitAmount(e.target.value)}
               InputProps={{
                 endAdornment: <InputAdornment position="end">ETH</InputAdornment>,
               }}
@@ -63,6 +90,7 @@ export default function Home() {
               id="outlined-helperText"
               placeholder="4"
               helperText="Valid Through"
+              onChange={(e) => setValidThrough(e.target.value)}
               InputProps={{
                 endAdornment: <InputAdornment position="end">Hour(s)</InputAdornment>,
               }}
@@ -75,6 +103,7 @@ export default function Home() {
             borderRadius: 8,
           }}
           variant="contained"
+          //onClick=await ethRegistrar.createCommit({commitDescription, commitTo, commitAmount, validThrough})
           >
           Commit
         </Button>
