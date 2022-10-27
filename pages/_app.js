b3216c0cd7ac9989929955c5e57515241d6a008e
Daniel Belfort <daniel@belfort.com>
2022-10-27 22:02:22 +0000

    removed everything except Header and Form & re-styled input boxes

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import '../styles/globals.css'
 import "@rainbow-me/rainbowkit/styles.css"
-import { getDefaultWallets, RainbowKitProvider, lightTheme } from '@rainbow-me/rainbowkit'
+import { getDefaultWallets, RainbowKitProvider, lightTheme as lightThemeRainbowkit } from '@rainbow-me/rainbowkit'
 import { chain, configureChains, createClient, WagmiConfig } from 'wagmi'
 import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from 'wagmi/providers/public'
-import PlausibleProvider from 'next-plausible'
+import { ThemeProvider } from 'styled-components'
+import { ThorinGlobalStyles, lightTheme as lightThemeENS } from '@ensdomains/thorin'
 
 const { chains, provider } = configureChains(
   [/*chain.mainnet,*/ chain.goerli],
@@ -22,27 +23,21 @@ const wagmiClient = createClient({
   provider
 });
 
-// always false until I register .xyz domain
-const isProdEnv = process.env.NEXT_PUBLIC_VERCEL_ENV == 'production'
-
 const App = ({ Component, pageProps }) => {
   return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider
-        chains={chains}
-        theme={lightTheme({
-          accentColor: '#1DD297',
-        })}
-      >
-        <PlausibleProvider
-            domain={isProdEnv ? 'prod-xyz-here' : 'phone-awareness-dapp.danielbelfort.repl.co'}
-            trackLocalhost={!isProdEnv}
-            trackOutboundLinks
+    <ThemeProvider theme={lightThemeENS}>
+      <ThorinGlobalStyles />
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider
+          chains={chains}
+          theme={lightThemeRainbowkit({
+            accentColor: '#1DD297',
+          })}
         >
           <Component {...pageProps} />
-        </PlausibleProvider>
-      </RainbowKitProvider>
-    </WagmiConfig>
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </ThemeProvider>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -2,35 +2,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { useState, useEffect } from 'react'
-import abi from "../contracts/CommitManager.json";
+import abi from "../contracts/CommitManager.json"
 import { ethers } from 'ethers'
-import TextField from '@mui/material/TextField';
-import InputAdornment from '@mui/material/InputAdornment';
-import Button from '@mui/material/Button';
+import { Input } from '@ensdomains/thorin'
+import Button from '@mui/material/Button'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import toast, { Toaster } from 'react-hot-toast'
-import Spinner from "../components/Spinner";
 import { useAccount, useNetwork, useProvider } from 'wagmi'
-import { useContractRead, useContractWrite, usePrepareContractWrite } from 'wagmi'
+import { useContractWrite, usePrepareContractWrite } from 'wagmi'
 import { useWaitForTransaction } from 'wagmi'
 import dayjs from "dayjs";
-import CommitCard from '../components/CommitCard'
-import CardList from '../components/CardList'
-
-const mockCommit = {
-  commitTo: "0x44B274C577e217409e6814C2667e78Ba987FBEEF",
-  commitFrom: "0x44B274C577e217409e6814C2667e78Ba987F30AD",
-  stakeAmount: "0.02",
-  createdTimestamp: "3 hrs ago (Sep-30-2022 04:31:45 AM +UTC)",
-  validPeriod: "24 hrs",
-}
 
 export default function Home() {
-
-  useEffect(() => {
-    // 👇️ only runs once
-    buildCommitArray();
-  }, []); // 👈️ empty dependencies array
 
   // state variables
   const [dialogOpen, setDialogOpen] = useState(false)
@@ -38,13 +21,6 @@ export default function Home() {
   const [commitTo, setCommitTo] = useState('');
   const [commitAmount, setCommitAmount] = useState('0')
   const [validThrough, setValidThrough] = useState('0')
-
-  // commit structure:
-  // id
-  // commitFrom
-  // commitTo
-
-  const [commitArray, setCommitArray] = useState([])
 
   // smart contract data
   const provider = useProvider()
@@ -60,55 +36,13 @@ export default function Home() {
 
   })
   const { data, isLoading, isSuccess, write } = useContractWrite(config)
-
-  const { data: commitData, isError, isLoading: commitIsLoading } = useContractRead({
-    addressOrName: contractAddress,
-    contractInterface: abi.abi,
-    functionName: "getAllCommits",
-  })
-
-  function buildCommitArray() {
-    // TODO: sort by expiry timestamp
-    let newArray = [];
-    for ( let commit of commitData) {
-      let newCommitStruct = {}
-      let eTimestamp = commit.expiryTimestamp.toNumber();
-      let status = "Failure";
-      if (commit.commitApproved) {
-        status = "Success";
-      } else if ( eTimestamp > Date.now()/1000 && commit.proofIpfsHash == "" ) {
-        status = "Pending";
-      } else if ( eTimestamp > Date.now()/1000 && commit.proofIpfsHash !== "" ) {
-        status = "Waiting";
-      } 
-      newCommitStruct.status = status;
-      newCommitStruct.userIsCreator = commit.commitFrom == isConnected;
-      newCommitStruct.userIsCommitee = commit.commitTo == isConnected;
-      console.log(isConnected)
-      console.log(newCommitStruct.userIsCreator)
-      newCommitStruct.expiryTimestamp = eTimestamp;
-      newCommitStruct.commitFrom = commit.commitFrom;
-      newCommitStruct.commitTo = commit.commitTo;
-      newCommitStruct.stakeAmount = commit.stakeAmount;
-      newCommitStruct.createdTimestamp = "TODO";
-      newCommitStruct.validPeriod = "TODO";
-      newCommitStruct.message =commit.message;
-
-      newArray.push(newCommitStruct);
-    }
-
-    newArray.sort((a, b) => (a.expiryTimestamp > b.expiryTimestamp) ? 1 : -1)
-    setCommitArray(newArray);
-  }
-
-
+  
   return (
     <>
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width" />
-        <title>JustCommit</title>
-
+        <title>Just Commit</title>
         <meta property="og:title" content="Just Commit" />
         <meta name="description" content="Just Commit" />
         <meta property="og:description" content="Just Commit" />
@@ -116,14 +50,12 @@ export default function Home() {
       </Head>
 
       <div className="header header--absolute bg-white">
-       <a href="./">
-         <img src="./logo.png"/>
-       </a>
-       <div>
-         <ConnectButton className="mr-2 md:inline-flex hover:shadow-lg flex" />
-       </div>
-      
-
+        <a href="./">
+          <img src="./logo.png"/>
+        </a>
+        <div>
+          <ConnectButton className="mr-2 md:inline-flex hover:shadow-lg flex" />
+        </div>
       </div>
 
       <div className="container container--flex container--one">
@@ -134,40 +66,48 @@ export default function Home() {
           className="form"
           onSubmit={async (e) => {
             e.preventDefault()
-            // do Toast checks 
+            // do Toast checks
             setDialogOpen(true)
           }}      
         >
           <div className="col">
-            <TextField
-              id="outlined-helperText"
-              placeholder="focused building"
-              helperText="Description"
+            <Input
+              label="Commitment"
+              maxLength={140}
+              placeholder="phone awareness"
+              parentStyles = {{ backgroundColor: '#fff' }}
               onChange={(e) => setCommitDescription(e.target.value)}
+              required
             />
-            <TextField
-              id="outlined-helperText"
-              placeholder="0xb44...aad"
-              helperText="Commit To"
+            <Input
+              label="Commit To"
+              maxLength={42}
+              placeholder="0xb443...9aad"
+              parentStyles = {{ backgroundColor: '#fff' }}
               onChange={(e) => setCommitTo(e.target.value)}
+              required
             />
-            <TextField
-              id="outlined-helperText"
+            <Input
+              label="Amount"
               placeholder="0.001"
-              helperText="Commit Amount"
+              step = "0.001"
+              min = {0}
+              type="number"
+              units="ETH"
+              parentStyles = {{ backgroundColor: '#fff' }}
               onChange={(e) => setCommitAmount(e.target.value)}
-              InputProps={{
-                endAdornment: <InputAdornment position="end">ETH</InputAdornment>,
-              }}
+              required
             />
-            <TextField
-              id="outlined-helperText"
-              placeholder="4"
-              helperText="Valid Through"
+            <Input
+              label="Duration"
+              placeholder="2"
+              type="number"
+              units="hours"
+              min={1}
+              max={10}
+              parentStyles={{ backgroundColor: '#fff' }}
               onChange={(e) => setValidThrough(e.target.value * 3600 + dayjs())}
-              InputProps={{
-                endAdornment: <InputAdornment position="end">Hour(s)</InputAdornment>,
-              }}
+              required
             />
           </div>
           
@@ -187,33 +127,15 @@ export default function Home() {
           </Button>
           )}
 
-          {/*|| (transactionStatus == "loading")*/}
+          {/*
           {isLoading && (
             <div className="justifyCenter">
               <Spinner />
             </div>
           )}
+          */}
           
         </form>
-      </div>
-      <div className='flex-col mr-4 ml-4 fd-col'>
-        {/* /* debug button */}
-        {/* <Button style ={{
-            width: '18%',
-            margin: '1rem',
-            backgroundColor: "#1DD297",
-            borderRadius: 8,
-          }}
-          variant="contained"
-          onClick={buildCommitArray}
-          >
-          fetch commit data
-        </Button> */}
-        <CardList cardList={commitArray}></CardList>
-        {/* <CommitCard status="Waiting" timeStamp={1665267459} commitFrom={mockCommit.commitFrom} commitTo={mockCommit.commitTo} stakeAmount={mockCommit.stakeAmount} createdTimestamp={mockCommit.createdTimestamp} validPeriod={mockCommit.validPeriod}/>
-        <CommitCard status="Success" timeStamp={1665267459} commitFrom={mockCommit.commitFrom} commitTo={mockCommit.commitTo} stakeAmount={mockCommit.stakeAmount} createdTimestamp={mockCommit.createdTimestamp} validPeriod={mockCommit.validPeriod}/>
-        <CommitCard status="Failure" timeStamp={1665267459} commitFrom={mockCommit.commitFrom} commitTo={mockCommit.commitTo} stakeAmount={mockCommit.stakeAmount} createdTimestamp={mockCommit.createdTimestamp} validPeriod={mockCommit.validPeriod}/>
-        <CommitCard status="Pending" timeStamp={1665267459} commitFrom={mockCommit.commitFrom} commitTo={mockCommit.commitTo} stakeAmount={mockCommit.stakeAmount} createdTimestamp={mockCommit.createdTimestamp} validPeriod={mockCommit.validPeriod}/> */}
       </div>
 
       {/*
@@ -226,9 +148,3 @@ export default function Home() {
     </>
   )
 }
-
-{/*
-(TODO: update to include the @media overrides)
-(TODO: figure out how to do tailwindCSS for below header CSS code)
-(TODO: add "Home" and "Feed" tabs)
-*/}
