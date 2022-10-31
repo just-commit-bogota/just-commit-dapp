d0c6a55d4b8a380a1b03b13323936d10d1dcc7cf
Daniel Belfort <daniel@belfort.com>
2022-10-31 04:25:54 +0000

    add *What is this?* button

--- pages/index.js
+++ pages/index.js
@@ -131,7 +131,7 @@ export default function Home() {
           </Button>
           )}
 
-          {/*What is this? button*/}
+          {/*What is this? button */}
           <a
             href = "https://danielbelfort.notion.site/Just-Commit-9213dcd452184278a4f628b0e3f48e78"
             target="_blank">
