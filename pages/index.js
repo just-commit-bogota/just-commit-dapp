ed737610c6f16d5fdaeeb79cd1fa86b03aead587
dbeic30-1 <116864463+dbeic30-1@users.noreply.github.com>
2022-11-02 00:44:46 -0400

    Update index.js
    add rel="noopener noreferrer" to link

--- pages/index.js
+++ pages/index.js
@@ -134,7 +134,8 @@ export default function Home() {
           {/*What is this? button*/}
           <a
             href = "https://danielbelfort.notion.site/Just-Commit-9213dcd452184278a4f628b0e3f48e78"
-            target="_blank">
+            target="_blank"
+            rel="noopener noreferrer">
             <u className= "text-sm text-sky-600 font-medium">         
               What is this?
             </u>
