7298e60568070cf6e0cd19152abe4d5c3f887e3e
Daniel Belfort <daniel@belfort.com>
2022-11-10 19:45:21 -0500

    Update feed.js

--- pages/feed.js
+++ pages/feed.js
@@ -1,11 +1,25 @@
+import Head from 'next/head'
 import Header from "../components/Header.js"
 
 export default function Feed() {
   return (
     <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width" />
+        <title>Just Commit</title>
+        <meta property="og:title" content="Just Commit" />
+        <meta name="description" content="Just Commit" />
+        <meta property="og:description" content="Just Commit" />
+        <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16.ico" />
+      </Head>
+      
       <Header />
-      <h2>Feed Page</h2>
+      <div className = "w-9/10 sm:w-1/2 mx-auto mt-20 p-8">
+        <div className = "text-2xl font-semibold flex justify-center">
+          ⏳ Working on it.
+        </div>
+      </div>
     </>
   )
 }
-
