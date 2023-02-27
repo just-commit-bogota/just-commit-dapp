dd83de9588ae750e4a2a061d8ce0c1183d905e3d
Daniel Belfort <daniel@belfort.com>
2023-02-27 00:28:15 +0000

    feat: treasury table draft (& commented out)

--- pages/treasury.js
+++ pages/treasury.js
@@ -5,6 +5,7 @@ import { Heading, Typography } from '@ensdomains/thorin'
 import { ethers } from 'ethers'
 import { CONTRACT_ADDRESS, ABI } from '../contracts/CommitManager.ts'
 import { useProvider } from 'wagmi'
+import toast, { Toaster } from 'react-hot-toast'
 
 export default function Treasury() {
 
@@ -39,7 +40,6 @@ export default function Treasury() {
 
   useEffect(() => {
     getSmartContractBalance()
-    console.log(balanceContract)
   }, [])
 
   // rendering
@@ -59,7 +59,7 @@ export default function Treasury() {
 
       <div className="flex h-screen">
         <div className="container container--flex">
-{/*           <CurrencyToggle /> */}
+          {/* <CurrencyToggle /> */}
           <div className="flex flex-row gap-5 mt-4">
             <div className="flex flex-col justify-center mt-4">
               <img className="h-16" src="./polygon-logo-tilted.svg" />
@@ -70,11 +70,42 @@ export default function Treasury() {
               </Heading>
             </div>
           </div>
-          <div className="mt-2 italic">
+          <div className="italic">
             <Typography weight="light" font="sans" style={{ lineHeight: "1.5" }}>
               Total amount of money in the smart contract<br />(from failed commitments).
             </Typography>
           </div>
+          {/* Table
+          <div class="mt-10 border border-gray-300 p-1"
+               style = {{minHeight: "50vh", minWidth: "80vw"}}
+          >
+            <div>
+              <div className="flex flex-row text-center text-sm gap-2 mt-2">
+                <div class="w-3/5">Heterodox Academy</div>
+                <div className="flex flex-row w-1/5">
+                  <div className="flex flex-col align-center justify-center mr-1">
+                    <img className="h-4" src="./polygon-logo-tilted.svg" />
+                  </div>
+                  <div>20</div>
+                </div>
+                <div class="w-1/10">
+                  <a className="w-1/2 justify-center font-medium text-blue-600 align-center p-0.5
+                                text-l rounded-lg bg-sky-200 hover:bg-sky-400 hover:cursor-pointer"
+                    onClick={() => { toast("⏳ Coming Soon...", { id: 'unique' }) }}>
+                      &nbsp;&nbsp;&nbsp;🗳️&nbsp;&nbsp;&nbsp;
+                  </a>
+                </div>
+                <div className="w-1/10">
+                  <a className="w-1/2 justify-center font-medium text-blue-600 align-center p-0.5
+                                text-l rounded-lg bg-sky-200 hover:bg-sky-400 hover:cursor-pointer"
+                    onClick={() => { toast("⏳ Coming Soon...", { id: 'unique' }) }}>
+                      &nbsp;&nbsp;&nbsp;🔎&nbsp;&nbsp;&nbsp;
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        */ }
         </div>
       </div>
     </>
