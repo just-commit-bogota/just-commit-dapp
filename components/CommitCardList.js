2556a54fbe6a915ce6e686764d14721a30df5b91
shitikyan <shitikyan@gmail.com>
2023-04-27 15:17:34 +0400

    Fixed Glitchy UI issue about scroll

--- components/CommitCardList.js
+++ components/CommitCardList.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import PullToRefresh from "react-simple-pull-to-refresh"
 import CommitCard from "./CommitCard.js"
 import { ethers } from 'ethers'
 import { useAccount } from 'wagmi'
@@ -66,69 +67,78 @@ export default function CommitCardList({ cardList }) {
 
   return (
     <>
-      <div className="flex justify-center gap-2 lg:gap-16 text-small mt-4 mb-10">
-       <ul className="flex flex-row continent_nav">
-        {filters.map(f =>
-          // HYDRATION ERROR (f !== "Verify" || (f === "Verify" && connectedAddress?.toUpperCase() === CONTRACT_OWNER.toUpperCase())) && (
-            <li key={f} id={f} title={f} className="filterOption"
-              style={{
-                position: "relative",
-                borderColor: (f == "Active" || f == "Waiting" || f == "Verify") ?
-                  "rgba(18, 74, 56, .5)" : "rgba(36, 41, 46, 0.8)",
-                borderWidth: "2px",
-                cursor: "pointer"
-              }}>
-              <a onClick={() => onFilterClick(f)}>{f}</a>
-              {/* Counter Badge */}
-              {["Active", "Waiting", "Verify"].includes(f) && filterCounts.find(filterCount => filterCount.filter === f)?.count > 0 &&
-                <Tag
-                  className="hover:cursor-pointer"
-                  size="small"
-                  style={{
-                    position: "absolute",
-                    top: "-10px",
-                    right: "-8px",
-                    color: "rgba(255, 255, 255, 1)",
-                    backgroundColor: f == "Waiting" ? "rgba(255, 201, 67, 1)" : "rgba(255, 90, 90, 1)"
-                  }}
-                >
-                  {filterCounts.find(filterCount => filterCount.filter === f).count}
-                </Tag>
-              }
-            </li>
-        )}
-      </ul>
-      </div>
-
-      <div className="w-full">
-        {cardListToDisplay.length > 0 ? (
-          <>
-            {cardListToDisplay.map((card) => (
-              <CommitCard       
-                key={card.id}
-                status={card.status}
-                id={card.id}
-                commitFrom={card.commitFrom}
-                commitJudge={card.commitJudge}
-                createdAt={card.createdAt}
-                endsAt={card.endsAt}
-                judgeDeadline={card.judgeDeadline}
-                phonePickups={card.phonePickups}
-                stakeAmount={ethers.utils.formatEther(card.stakeAmount)}
-                filename={card.filename}
-                isCommitProved={card.isCommitProved}
-                isCommitJudged={card.isCommitJudged}
-                isApproved={card.isApproved}
-              />
-            )).reverse()}
-          </>
-        ) : (
-          <Typography weight="normal" variant="base" className="flex flex-row text-m block mt-6 text-black font-bold rounded-md p-3 lg:justify-center lg:align-center"
-            style={{ justifyContent: "center" }}>
-            Nothing to show.
-          </Typography>
-        )}
-      </div>
+      <PullToRefresh
+        onRefresh={() => {
+          try {
+            return location.reload();
+          } catch (error) {
+            return;
+          }
+        }}
+      >
+        <div className="flex justify-center gap-2 lg:gap-16 text-small mt-4 mb-10">
+         <ul className="flex flex-row continent_nav">
+          {filters.map(f =>
+            // HYDRATION ERROR (f !== "Verify" || (f === "Verify" && connectedAddress?.toUpperCase() === CONTRACT_OWNER.toUpperCase())) && (
+              <li key={f} id={f} title={f} className="filterOption"
+                style={{
+                  position: "relative",
+                  borderColor: (f == "Active" || f == "Waiting" || f == "Verify") ?
+                    "rgba(18, 74, 56, .5)" : "rgba(36, 41, 46, 0.8)",
+                  borderWidth: "2px",
+                  cursor: "pointer"
+                }}>
+                <a onClick={() => onFilterClick(f)}>{f}</a>
+                {/* Counter Badge */}
+                {["Active", "Waiting", "Verify"].includes(f) && filterCounts.find(filterCount => filterCount.filter === f)?.count > 0 &&
+                  <Tag
+                    className="hover:cursor-pointer"
+                    size="small"
+                    style={{
+                      position: "absolute",
+                      top: "-10px",
+                      right: "-8px",
+                      color: "rgba(255, 255, 255, 1)",
+                      backgroundColor: f == "Waiting" ? "rgba(255, 201, 67, 1)" : "rgba(255, 90, 90, 1)"
+                    }}
+                  >
+                    {filterCounts.find(filterCount => filterCount.filter === f).count}
+                  </Tag>
+                }
+              </li>
+          )}
+        </ul>
+        </div>
+        <div className="w-full">
+          {cardListToDisplay.length > 0 ? (
+            <>
+              {cardListToDisplay.map((card) => (
+                <CommitCard       
+                  key={card.id}
+                  status={card.status}
+                  id={card.id}
+                  commitFrom={card.commitFrom}
+                  commitJudge={card.commitJudge}
+                  createdAt={card.createdAt}
+                  endsAt={card.endsAt}
+                  judgeDeadline={card.judgeDeadline}
+                  phonePickups={card.phonePickups}
+                  stakeAmount={ethers.utils.formatEther(card.stakeAmount)}
+                  filename={card.filename}
+                  isCommitProved={card.isCommitProved}
+                  isCommitJudged={card.isCommitJudged}
+                  isApproved={card.isApproved}
+                />
+              )).reverse()}
+            </>
+          ) : (
+            <Typography weight="normal" variant="base" className="flex flex-row text-m block mt-6 text-black font-bold rounded-md p-3 lg:justify-center lg:align-center"
+              style={{ justifyContent: "center" }}>
+              Nothing to show.
+            </Typography>
+          )}
+        </div>
+      </PullToRefresh>
     </>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,19 @@
-import '../styles/globals.css'
-import "@rainbow-me/rainbowkit/styles.css"
-import { Analytics } from '@vercel/analytics/react';
-import { getDefaultWallets, RainbowKitProvider, lightTheme as lightThemeRainbowkit } from '@rainbow-me/rainbowkit'
-import { chain, configureChains, createClient, WagmiConfig } from 'wagmi'
-import { infuraProvider } from 'wagmi/providers/infura'
-import { publicProvider } from 'wagmi/providers/public'
-import { ThemeProvider } from 'styled-components'
-import { ThorinGlobalStyles, lightTheme as lightThemeENS } from '@ensdomains/thorin'
-import PullToRefresh from 'react-simple-pull-to-refresh';
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { Analytics } from "@vercel/analytics/react";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  lightTheme as lightThemeRainbowkit,
+} from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { infuraProvider } from "wagmi/providers/infura";
+import { publicProvider } from "wagmi/providers/public";
+import { ThemeProvider } from "styled-components";
+import {
+  ThorinGlobalStyles,
+  lightTheme as lightThemeENS,
+} from "@ensdomains/thorin";
 
 const { chains, provider } = configureChains(
   // [chain.polygon, chain.mainnet], // to ENS reverse resolve
@@ -16,7 +22,7 @@ const { chains, provider } = configureChains(
   //[chain.polygonMumbai], // DEV
 
   [infuraProvider({}), publicProvider()]
-)
+);
 
 const { connectors } = getDefaultWallets({
   appName: "Just Commit dApp",
@@ -31,30 +37,20 @@ const wagmiClient = createClient({
 
 const App = ({ Component, pageProps }) => {
   return (
-    <PullToRefresh
-      onRefresh={() => {
-        try {
-          return location.reload();
-        } catch (error) {
-          return;
-        }
-      }}
-    >
-      <ThemeProvider theme={lightThemeENS}>
-        <ThorinGlobalStyles />
-        <WagmiConfig client={wagmiClient}>
-          <RainbowKitProvider
-            chains={chains}
-            theme={lightThemeRainbowkit({
-              accentColor: "#1DD297",
-            })}
-          >
-            <Component {...pageProps} />
-            <Analytics />
-          </RainbowKitProvider>
-        </WagmiConfig>
-      </ThemeProvider>
-    </PullToRefresh>
+    <ThemeProvider theme={lightThemeENS}>
+      <ThorinGlobalStyles />
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider
+          chains={chains}
+          theme={lightThemeRainbowkit({
+            accentColor: "#1DD297",
+          })}
+        >
+          <Component {...pageProps} />
+          <Analytics />
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </ThemeProvider>
   );
 };
 
