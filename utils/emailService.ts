a2b4c21626ba8e4600ad24fe2ead13247f0bca61
shitikyan <shitikyan@gmail.com>
2023-05-04 11:50:18 +0400

    Changed logic for email

--- utils/emailService.ts
+++ utils/emailService.ts
@@ -1,9 +1,15 @@
 import emailjs from "emailjs-com";
 import env from "./env";
 
-export const sendAnEmail = (email: string): void => {
+export const sendAnEmail = (
+  email: string,
+  commitId?: string,
+  endsAt?: string
+): void => {
   const templateParams = {
     email,
+    commitId,
+    endsAt,
   };
 
   const { emailJsServiceId, emailJsTemplateId, emailJsPublicKey } = env;
