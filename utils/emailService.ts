5976a4e4ee0497b7aa3b2a4b42296de269e2b706
shitikyan <shitikyan@gmail.com>
2023-05-04 18:28:55 +0400

    Minor fix for email

--- utils/emailService.ts
+++ utils/emailService.ts
@@ -1,15 +1,9 @@
 import emailjs from "emailjs-com";
 import env from "./env";
 
-export const sendAnEmail = (
-  email: string,
-  commitId?: string,
-  endsAt?: string
-): void => {
+export const sendAnEmail = (email: string): void => {
   const templateParams = {
     email,
-    commitId,
-    endsAt,
   };
 
   const { emailJsServiceId, emailJsTemplateId, emailJsPublicKey } = env;
